045ea563757eb93ea8a372e0e11479c5c630a03f
尘心_无愿 <2568237200@qq.com>
2025-09-29 22:20:57 +0800

    [feat] 实现玩家角色WASD移动控制

--- assets/scripts/player/move.ts
+++ assets/scripts/player/move.ts
@@ -8,7 +8,31 @@ export class move extends Component {
     }
 
     update(deltaTime: number) {
-        
+        //wasd移动2d刚体
+        if (this.node.getComponent(RigidBody2D)) {
+            move()
+        }
+    }
+
+    move() {
+        const body = this.node.getComponent(RigidBody2D);
+            const velocity = body.linearVelocity;
+            velocity.x = 0;
+            velocity.y = 0;
+            if (input.isKeyDown(KeyCode.W)) {
+                velocity.y = 10;
+            }
+            if (input.isKeyDown(KeyCode.S)) {
+                velocity.y = -10;
+            }
+            if (input.isKeyDown(KeyCode.A)) {
+                velocity.x = -10;
+            }
+            if (input.isKeyDown(KeyCode.D)) {
+                velocity.x = 10;
+            }
+            body.linearVelocity = velocity;
     }
 }
-
+
+
