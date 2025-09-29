5ef06a36b75fa8fdc9170e69c387ad8e4bdf1cd8
尘心_无愿 <2568237200@qq.com>
2025-09-29 22:25:06 +0800

    [feat] 添加移动调试日志并优化移动控制逻辑

--- assets/scripts/player/move.ts
+++ assets/scripts/player/move.ts
@@ -9,24 +9,31 @@ export class move extends Component {
 
     update(deltaTime: number) {
         //wasd移动2d刚体
-        if (this.node.getComponent(RigidBody2D)) {
+        /* if (this.node.getComponent(RigidBody2D)) {
             move()
-        }
+        } */
+        //else {
+            move2d()
+       //}
     }
 
     //wasd移动2d图片
     move2d() {
         const pos = this.node.getPosition();
         if (input.isKeyDown(KeyCode.W)) {
+            console.log("移动了w"+pos.y)
             pos.y += 0.1;
         }
         if (input.isKeyDown(KeyCode.S)) {
+            console.log("移动了s"+pos.y)
             pos.y -= 0.1;
         }
         if (input.isKeyDown(KeyCode.A)) {
+            console.log("移动了a"+pos.x)
             pos.x -= 0.1;
         }
         if (input.isKeyDown(KeyCode.D)) {
+            console.log("移动了d"+pos.x)
             pos.x += 0.1;
         }
         this.node.setPosition(pos);
