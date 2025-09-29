3afec06c695bc5007227dce2d496015bcd7ccbf0
尘心_无愿 <2568237200@qq.com>
2025-09-29 22:22:00 +0800

    [feat] 添加2D图片WASD移动控制并优化刚体移动代码

--- assets/scripts/player/move.ts
+++ assets/scripts/player/move.ts
@@ -14,24 +14,43 @@ export class move extends Component {
         }
     }
 
+    //wasd移动2d图片
+    move2d() {
+        const pos = this.node.getPosition();
+        if (input.isKeyDown(KeyCode.W)) {
+            pos.y += 0.1;
+        }
+        if (input.isKeyDown(KeyCode.S)) {
+            pos.y -= 0.1;
+        }
+        if (input.isKeyDown(KeyCode.A)) {
+            pos.x -= 0.1;
+        }
+        if (input.isKeyDown(KeyCode.D)) {
+            pos.x += 0.1;
+        }
+        this.node.setPosition(pos);
+    }
+
+    //移动2d刚体
     move() {
         const body = this.node.getComponent(RigidBody2D);
-            const velocity = body.linearVelocity;
-            velocity.x = 0;
-            velocity.y = 0;
-            if (input.isKeyDown(KeyCode.W)) {
-                velocity.y = 10;
-            }
-            if (input.isKeyDown(KeyCode.S)) {
-                velocity.y = -10;
-            }
-            if (input.isKeyDown(KeyCode.A)) {
-                velocity.x = -10;
-            }
-            if (input.isKeyDown(KeyCode.D)) {
-                velocity.x = 10;
-            }
-            body.linearVelocity = velocity;
+        const velocity = body.linearVelocity;
+        velocity.x = 0;
+        velocity.y = 0;
+        if (input.isKeyDown(KeyCode.W)) {
+            velocity.y = 10;
+        }
+        if (input.isKeyDown(KeyCode.S)) {
+            velocity.y = -10;
+        }
+        if (input.isKeyDown(KeyCode.A)) {
+            velocity.x = -10;
+        }
+        if (input.isKeyDown(KeyCode.D)) {
+            velocity.x = 10;
+        }
+        body.linearVelocity = velocity;
     }
 }
 
